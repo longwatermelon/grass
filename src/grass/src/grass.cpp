12f8c8388700c7287caf7ed856a24c405d72034c
longwatermelon <longwatermelon1@gmail.com>
2021-06-16 14:43:57 -0700

    move fonts to font folder

--- src/grass/src/grass.cpp
+++ src/grass/src/grass.cpp
@@ -35,8 +35,8 @@ Grass::Grass(const std::string& exe_dir)
     TTF_Init();
     IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
 
-    m_font_textbox.load_font(m_exe_dir + "res/CascadiaCode.ttf", 16);
-    m_font_tree.load_font(m_exe_dir + "res/CascadiaCode.ttf", 14);
+    m_font_textbox.load_font(m_exe_dir + "res/fonts/CascadiaCode.ttf", 16);
+    m_font_tree.load_font(m_exe_dir + "res/fonts/CascadiaCode.ttf", 14);
 
     m_font_textbox.automatically_delete(false);
     m_font_tree.automatically_delete(false);
@@ -94,7 +94,7 @@ void Grass::mainloop()
 
     std::string renamed_fp;
 
-    gui::common::Font explorer_font(m_exe_dir + "res/CascadiaCode.ttf", 14);
+    gui::common::Font explorer_font(m_exe_dir + "res/fonts/CascadiaCode.ttf", 14);
 
     SDL_Point pos;
     SDL_GetWindowPosition(m_window, &pos.x, &pos.y);
